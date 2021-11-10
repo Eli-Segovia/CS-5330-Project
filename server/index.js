369e80eeebad846959fb6735f5a9ce8516476ef2
Eli <elisego46@gmail.com>
2021-11-10 13:30:53 -0600

    add some config stuff

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config({ path: './config/.env' });
+}
 
 const app = express();
 
@@ -6,6 +11,6 @@ app.get('/', (req, res) => {
     res.send('Hello, world');
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
