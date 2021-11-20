b413bba15464ec85a6c07da180bd42fedbaffcd0
Corey23 <corey.ellis23@icloud.com>
2021-11-19 22:25:19 -0600

    modelsmade

---
+++ models/authorModel.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+/**
+ * Create database scheme for notes
+ */
+const AuthorSchema = new Schema({
+    authorId: {
+        type: int,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+    },
+
+    firstName: {
+        type: String,
+        default: null,
+    },
+
+    affiliation: {
+        type: [{
+            name: {
+                type: String,
+                required: true,
+            },
+            start: {
+                type: Date,
+                required: true,
+            },
+            end: {
+                type: Date,
+                default: null,
+            }
+        }],
+        default: null,
+    }
+});
+
+export default mongoose.model('Author', AuthorSchema);
---
+++ models/paperModel.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+/**
+ * Create database scheme for notes
+ */
+const PaperSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+
+    authors: {
+        type: [String],
+        required: true,
+    },
+
+    conference: {
+        type: mongoose.ObjectId,
+        default: null,
+    },
+
+    journal: {
+        type: mongoose.ObjectId,
+        default: null,
+    },
+   
+    category: {
+        type: String,
+        required: false,
+        default: 'haircut',
+    },
+    url: {
+        type: String,
+        required: null,
+    },
+    page: {
+        type: String,
+        default: true,
+    }
+});
+
+export default mongoose.model('Paper', PaperSchema);
