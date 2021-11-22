11d89a64045395fd83cb98e06e4021d604cb9ae7
Eli <elisego46@gmail.com>
2021-11-22 00:16:18 -0600

    Fix file structre and delete some unneeded files
    Went throug client folder and server folder and modified some
    configuration stuff as well as removed unneeded files from the client
    dir.
    
    Also added some dirs for our workflow:
    - the styles dir is for all of our CSS
    - the assets dir is for any images we might want to store
    - the components dir is where we will put all of our own components

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,27 +1,27 @@
 /* global module */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    // suppress errors for missing 'import React' in files
-    'react/react-in-jsx-scope': 'off',
-    // allow jsx syntax in js files (for next.js project)
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 11,
+        sourceType: 'module'
     },
-  },
+    plugins: ['react'],
+    rules: {
+        // suppress errors for missing 'import React' in files
+        'react/react-in-jsx-scope': 'off',
+        // allow jsx syntax in js files (for next.js project)
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }] //should add ".ts" if typescript project
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
 };
